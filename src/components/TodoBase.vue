ea9ab90655dbb560a1a620fb496e9ace03e3b062
Andras Soskuthy <asoskuthy@mutualvision.co.uk>
2018-10-23 15:09:59 +0100

    TodoBase basic logic finished

--- src/components/TodoBase.vue
+++ src/components/TodoBase.vue
@@ -7,8 +7,10 @@
         v-model="poNumber"                    
         label="Enter PO"
         :append-icon="isValidPO"
+        
             outline>
          </v-text-field> 
+         <transition name="fade">
         <v-text-field color="teal"    
         v-model="poDescription"
         v-if="poNumber"            
@@ -16,20 +18,33 @@
         :append-icon="isValidDescription"
             outline>
          </v-text-field> 
-           <v-list>
+         </transition>
+         <transition name="fade">
+        
+        <v-list v-if="poDescription">
+         
+          <v-chip :class="isBlackOrWhite" :color="areTodosFinished">{{ doneTodos }} / {{ todos.length}}</v-chip>
+      
          <v-list-tile   
             v-for="item in todos"
             :key="item.id"                       
           >         
            <v-list-tile-avatar>
-              <v-checkbox v-model="item.checked" color="teal"  ></v-checkbox>
+              <v-checkbox color="teal"  v-model="item.checked"></v-checkbox>
             </v-list-tile-avatar>
 
             <v-list-tile-content>
-              <v-list-tile-title color="teal" class="mt-1" v-text="item.name"></v-list-tile-title>
+              <v-list-tile-title  class="mt-1" :class="{ 'done-todo': item.checked }" v-text="item.name"></v-list-tile-title>
             </v-list-tile-content>
           </v-list-tile>
         </v-list>
+    </transition>
+    <v-btn   v-if="poDescription && poNumber" dark color="teal">
+        <v-icon dark>save</v-icon>
+    </v-btn>    
+    <v-btn   v-if="poDescription && poNumber" dark color="teal">
+      <v-icon dark>edit</v-icon>
+    </v-btn>
         </v-card>
         </v-flex> 
     </v-layout>    
@@ -42,11 +57,20 @@ data() {
     return {       
         poNumber: null,
         poDescription: null,
-        todos: [],
-        test: false
+        todos: []       
     }    
 },
 computed: {    
+    isBlackOrWhite(){
+        return this.doneTodos === this.todos.length ? 'white' : ''
+    },
+    areTodosFinished(){
+          return this.doneTodos === this.todos.length ? 'teal': ''           
+    },
+    doneTodos() {
+        let todos = this.todos.filter((todo)=> (todo.checked))
+        return todos.length
+    },
     isValidPO() {
         return this.poNumber ? 'done': ''     
     },
@@ -56,24 +80,53 @@ computed: {
 },
 watch: {
     'poNumber'() {
-         if(this.poNumber === '') {             
+         if(!this.poNumber) {             
              this.poDescription = null
+             this.todos = []
+         }else{
+             this.fetchTodos()
          }
     }
 },
-created() {
-    let todo = {
+methods:{
+    fetchTodos() {
+        if(this.todos.length > 0)
+            return 
+
+        let todo = {
         id: 1,
         checked: false,
-        name: 'Im a todo'
+        name: 'JIRA ticket status changed, assigned to developer'
+    }
+    let todo2 = {
+        id: 2,
+        checked: false,
+        name: 'Feature or Hotfix branch created'
     }
     this.todos.push(todo)
-    
-    
+    this.todos.push(todo2)
+    }
+},
+created(){
+    this.fetchTodos()
 }
 }
 </script>
 
-<style>
-
+<style scoped>
+.white{
+    color: white;
+}
+.black {
+    color: black;
+}
+.done-todo {
+    text-decoration: line-through;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
