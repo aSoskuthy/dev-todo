488fc999ccf1453654ee95c6b26b524c038483c2
Andras Soskuthy <asoskuthy@mutualvision.co.uk>
2018-10-23 10:06:51 +0100

    Removed cli defaults

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Checklist</router-link> |
+      <router-link to="/about">Account</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Checklist.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="checklist">   
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'checklist'  
+}
+</script>
